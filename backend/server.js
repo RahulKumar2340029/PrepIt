bf88b464281862b4b869b441bbdd1a6bc7a90d97
RahulKumar2340029 <143934828+RahulKumar2340029@users.noreply.github.com>
2025-07-25 00:53:11 +0530

    ai controller made , backend completed

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const connectDB = require("./config/db")
 const authRoutes = require('./routes/auth.routes')
 const sessionRoutes = require('./routes/session.routes')
 const questionRoutes = require('./routes/question.routes')
+const { protect } = require('./middlewares/authMiddleware') 
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/ai.controller")
 const app = express()
 
 // Middleware to handle cors
@@ -30,8 +32,8 @@ app.use("/api/auth",authRoutes)
 app.use("/api/sessions",sessionRoutes)
 app.use("/api/questions",questionRoutes)
 
-// app.use("api/ai/generate-questions",protect,generateInterviewQuestions)
-// app.use("/api/ai/generate-explanation",protect,generateConceptExplanation)
+app.use("/api/ai/generate-questions",protect,generateInterviewQuestions)
+app.use("/api/ai/generate-explanation",protect,generateConceptExplanation)
 
 
 // Serve uploads folder
